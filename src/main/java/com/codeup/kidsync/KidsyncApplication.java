6ed3b0bdb5aebe0ad536f0f8d764605b02d444bf
Elvin <ttop34@icloud.com>
2017-11-16 17:01:43 -0600

    initial commit

---
+++ src/main/java/com/codeup/kidsync/KidsyncApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.kidsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsyncApplication.class, args);
+	}
+}
