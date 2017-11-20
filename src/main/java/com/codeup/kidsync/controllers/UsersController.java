ef151d6af224deb2c9c700d101050c37c570904f
Elvin <ttop34@icloud.com>
2017-11-20 09:17:19 -0600

    working on getting users saved to db

--- src/main/java/com/codeup/kidsync/controllers/UsersController.java
+++ src/main/java/com/codeup/kidsync/controllers/UsersController.java
@@ -1,8 +1,35 @@
 package com.codeup.kidsync.controllers;
 
+import com.codeup.kidsync.models.User;
+import com.codeup.kidsync.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 public class UsersController {
+    private UsersRepository users;
+    private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    public UsersController(UsersRepository users, PasswordEncoder passwordEncoder) {
+        this.users = users;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/register")
+    public String showRegisterForm(Model model){
+        model.addAttribute("user", new User());
+        return "users/register";
+    }
+
+    @PostMapping("/register")
+    public String saveUser(@ModelAttribute User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        users.save(user);
+        return "redirect:/login";
+    }
 }
--- src/main/java/com/codeup/kidsync/repositories/UsersRepository.java
+++ src/main/java/com/codeup/kidsync/repositories/UsersRepository.java
@@ -4,5 +4,5 @@ import com.codeup.kidsync.models.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsersRepository extends CrudRepository<User, Long> {
-    User findByUsername(String username);
+    User findByEmail(String email);
 }
