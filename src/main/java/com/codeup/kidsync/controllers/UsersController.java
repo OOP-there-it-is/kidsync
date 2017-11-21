b22e35c5d00f43add53aa124341394a9b6973de6
Elvin <ttop34@icloud.com>
2017-11-21 11:04:56 -0600

    working on method to send invitation code

--- src/main/java/com/codeup/kidsync/controllers/UsersController.java
+++ src/main/java/com/codeup/kidsync/controllers/UsersController.java
@@ -2,17 +2,23 @@ package com.codeup.kidsync.controllers;
 
 import com.codeup.kidsync.models.User;
 import com.codeup.kidsync.repositories.UsersRepository;
+import com.codeup.kidsync.twillio.SendSms;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class UsersController {
     private UsersRepository users;
     private PasswordEncoder passwordEncoder;
+    private SendSms send;
+
 
     @Autowired
     public UsersController(UsersRepository users, PasswordEncoder passwordEncoder) {
@@ -58,5 +64,18 @@ public class UsersController {
     public String showSignUp(Model model){
         model.addAttribute("user", new User());
         return "users/signUp";
+    }
+
+    @GetMapping("/sms")
+    public String showForm(Model vModel){
+        vModel.addAttribute("user", users.findAll());
+        return "users/sms";
+    }
+
+    @PostMapping("/sms")
+    public String sendSms(HttpServletRequest request){
+        String phone = request.getParameter("phone");
+        System.out.println(send.sendCode(phone));
+        return "redirect:/login";
     }
 }
--- src/main/java/com/codeup/kidsync/twillio/SendSms.java
+++ src/main/java/com/codeup/kidsync/twillio/SendSms.java
@@ -1,26 +1,20 @@
 package com.codeup.kidsync.twillio;
 
-import com.codeup.kidsync.Config;
-import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
-
-
 public class SendSms  {
 
-
-
-    // Find your Account Sid and Token at twilio.com/user/account
-
-
-    public static void main(String[] args) {
-        Twilio.init(Config.getAccountSid(), Config.getAuthToken());
+    public Message sendCode(String phone){
+        int msg = (int) (Math.random() * 5000 + 2000);
+         String newMsg = Integer.toString(msg);
 
         Message message = Message.creator(
-                new PhoneNumber("+14029577924"),
+                new PhoneNumber("+1" + phone),
                 new PhoneNumber("+17123555738"),
-                "test"
+                newMsg
         ).create();
+
+        return message;
     }
 }
